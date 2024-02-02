fea485d50b0a4fcb738e4912916420852933e97c
Karine Franco Delanegri <you@example.comk.delanegri@gmail.com>
2024-02-01 21:02:34 -0300

    reader_instalacao de funcionalidade de botao de pause/play e do botao avancar

---
+++ script.js
@@ -0,0 +1,45 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
+const botaoAvancar = document.getElementById('proximo');
+
+const numeroCapitulos = 10;
+let audioTocando = 0; /*vai checar se esá tocando, inicia sem tocar com o O*/
+let capituloAtual = 1;
+
+function tocarFaixa() {
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-fill'); /*alterando o botão de play por pause*/
+    botaoPlayPause.classList.add('bi-pause-fill');
+}
+
+function pausarFaixa() {
+    audioCapitulo.pause();
+    botaoPlayPause.classList.remove('bi-pause-fill'); /*alterando o botão de pause por play*/
+    botaoPlayPause.classList.add('bi-play-fill');
+}
+
+function tocarOuPausar() {
+    if (audioTocando === 0) {
+        tocarFaixa();
+        audioTocando = 1;
+    } else {
+        pausarFaixa();
+        audioTocando = 0;
+    }
+}
+
+function proximaFaixa() {
+    if ( capituloAtual === numeroCapitulos ) {
+        capituloAtual = 1;
+    } else {
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    audioTocando = 1;
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+
+botaoAvancar.addEventListener('click', proximaFaixa);
